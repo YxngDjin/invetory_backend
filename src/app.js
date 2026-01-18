83957ec2da15280b3711a636c9c724139b98a0f4
Dennis Lehmann <wuestefelddennis@gmail.com>
2026-01-18 16:31:37 +0100

    Implement Server Configurations for ArcJet Neon Drizzle and Winston logger

---
+++ src/app.js
@@ -0,0 +1,43 @@
+import express from 'express';
+import logger from './config/logger.js';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+
+
+const app = express();
+
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.use(
+  morgan('combined', {
+    stream: { write: message => logger.info(message.trim()) },
+  })
+);
+
+// Middlewares
+
+
+app.get('/', (req, res) => {
+  logger.info('Hello from Inventory Backend!');
+  res.status(200).send('Hello from Inventory Backend!');
+});
+
+app.get('/health', (req, res) => {
+  res.status(200).json({
+    status: 'OK',
+    timestamp: new Date().toISOString(),
+    uptime: process.uptime(),
+  });
+});
+
+app.use((req, res) => {
+  res.status(404).json({ error: 'Not Found' });
+});
+
+export default app;
---
+++ src/config/database.js
@@ -0,0 +1,19 @@
+import 'dotenv/config';
+import { neon, neonConfig } from '@neondatabase/serverless';
+import { drizzle } from 'drizzle-orm/neon-http';
+
+if (process.env.NODE_ENV === 'development') {
+  neonConfig.fetchEndpoint = 'https://neon-local:5432/sql';
+  neonConfig.useSecureWebSocket = false;
+  neonConfig.poolQueryViaFetch = true;
+  console.log('In Development Mode. Using Neon Local');
+}
+
+const sql = neon(process.env.DATABASE_URL);
+
+const db = drizzle(sql);
+
+export {
+  db,
+  sql
+};
